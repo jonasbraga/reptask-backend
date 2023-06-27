92a1b6d25626807ae62ad17cb131948ad283a9fd
Jonas Braga <jonasbraga2001@gmail.com>
2023-06-26 23:11:33 -0300

    style: :lipstick: eslint and prettier fixes

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,18 @@
-import { createConnection } from "typeorm";
-require('dotenv').config();
+import { createConnection } from 'typeorm'
+require('dotenv').config()
 
-export async function connect() {
-     const conn = createConnection({
-        name: 'default',
-        type: 'postgres',
-        host: process.env.DATABASE_IP,
-        port: parseInt(process.env.DATABASE_PORT),
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASS,
-        database: process.env.DATABASE_NAME,
-        logging: true
-      //   entities: ['src/database/models/*.ts']
-     });
+export async function connect () {
+  const conn = createConnection({
+    name: 'default',
+    type: 'postgres',
+    host: process.env.DATABASE_IP,
+    port: Number.parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    logging: true,
+    //   entities: ['src/database/models/*.ts']
+  })
 
-    return conn;
+  return conn
 }
-
