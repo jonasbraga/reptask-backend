45d7e9c2fbbf3e5d7e94869a404e6a202f26c21b
Jonas Braga <jonasbraga2001@gmail.com>
2023-06-27 12:19:44 -0300

    build: :bricks: fixing docker and updating database init script

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,9 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors())
-app.use(express.json())
 app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ limit: '100mb' }))
 
 app.use(routes)
 
-app.listen(3000, () => console.log('\u{1F525} Server is running'))
+app.listen(process.env.NODE_PORT!, () => console.log(`\u{1F525} Server is running on port ${process.env.NODE_PORT!}`))
